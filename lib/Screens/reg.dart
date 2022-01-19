1e849cffe5d47184fab4228acb2b6dd55a0a6aad
WhiteHorse101 <rishabh.saudagar2001@gmail.com>
2022-01-19 10:12:26 +0530

    Login Screen Update

--- lib/Screens/reg.dart
+++ lib/Screens/reg.dart
@@ -18,13 +18,70 @@ class _regState extends State<reg> {
         children: <Widget>[
           Image.asset('assets/headerimg.jpg'),
           Container(
-            padding: EdgeInsets.fromLTRB(20, 40, 0, 0),
+            padding: EdgeInsets.fromLTRB(10, 40, 0, 0),
             child: Text(
-              "India's #1 Food Delivery \n  and Dining App",
+              "India's #1 Food Delivery",
               style: TextStyle(
                 fontFamily: 'Metropolis',
                 fontSize: 28,
                 fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              "and Dining App",
+              style: TextStyle(
+                  fontFamily: 'Metropolis',
+                  fontSize: 28,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+          Row(children: <Widget>[
+            Expanded(
+              // ignore: unnecessary_new
+              child: new Container(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                margin: const EdgeInsets.only(left: 10.0, right: 15.0),
+                child: Divider(
+                  color: Colors.grey[400],
+                  height: 50,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Text(
+                'Login in or sign up',
+                style: TextStyle(
+                    fontFamily: 'Metropolis',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+            Expanded(
+                // ignore: unnecessary_new
+                child: new Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              margin: const EdgeInsets.only(left: 11.0, right: 15.0),
+              child: Divider(
+                color: Colors.grey[400],
+                height: 50,
+              ),
+            )),
+          ]),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Material(
+              elevation: 5,
+              child: TextField(
+                textInputAction: TextInputAction.send,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
             ),
           ),
